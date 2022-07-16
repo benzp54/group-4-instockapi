f52fad7a4ba8ad47b376395400d75a6a7f3dfdd5
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2022-07-16 12:52:20 -0400

    get route working and able to get warehouse list

--- routes/warehouseList.js
+++ routes/warehouseList.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
+const warehouseData = require('../warehouses.json')
+
+const warehouseList = warehouseData.map(value => ({
+    id: value.id,
+    name: value.name,
+    address: value.address,
+    city: value.city,
+    country: value.country,
+    contact: value.contact
+}))
+
+console.log(warehouseList)
 
 router.get('/', (req, res) => {
-    res.send('hello there')
+    res.send(warehouseList)
     console.log('TEST2')
 })
 
